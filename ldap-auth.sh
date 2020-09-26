cfa578299a40b156f422ee4077a2a892b9b70e00
Teagan glenn <teagan.m.glenn@gmail.com>
2020-09-26 00:19:14 -0600

    Update auth script

--- ldap-auth.sh
+++ ldap-auth.sh
@@ -1,4 +1,104 @@
 #!/bin/sh
+
+#
+# ldap-auth.sh - Simple shell script to authenticate users against LDAP
+#
+# This script should be able to run on any POSIX-compliant shell with
+# cat, grep and sed available (even works on BusyBox).
+# It expects to get two environment variables: username and password of
+# the user to be authenticated.
+#
+# It then rewrites the username into a DN and tries to bind with it,
+# using the supplied password.
+#
+# When binding succeded, it can optionally execute a search to e.g. check
+# for group memberships or, alternatively, grant access straight away.
+#
+# When access is granted, it exits with an exit code of 0, non-zero
+# otherwise. Status messages are only written to stderr, not stdout,
+# so you can use stdout to pass whatever you want back to the caller.
+#
+# In order to run ldap-auth.sh, you need to create a configuration file
+# first. Either take a config from the examples directory or copy the
+# available settings below to a new file.
+# NOTE: The configuration file will be sourced, hence you can use regular
+# shell script syntax inside. Since the config file is in fact executed,
+# make sure it's content is safe.
+#
+# Finally, configure the service that wants to do LDAP authentication to
+# execute ldap-auth.sh and pass the configuration file as its only argument.
+#
+# Additionally, you may define the following functions in your config file,
+# which get then called at the corresponding events, before ldap-auth.sh
+# exits. You could print some additional info from these functions,
+# for instance.
+#
+#     on_auth_success() {
+#         ...
+#     }
+#
+#     on_auth_failure() {
+#         ...
+#     }
+#
+# In these functions, you have access to the $output variable,
+# which will hold the raw LDIF output produced by the LDAP client.
+# It won't include comments, but curl produces a somewhat invalid LDIF
+# with blank lines between different attributes and deep indentation.
+#
+
+########## CONFIGURATION ##########
+# Some settings (those not commented out) have default values set. You
+# don't need to include them in your configuration file unless you want
+# to change these defaults.
+
+# Uncomment to enable debugging to stderr (prints full client output
+# and more).
+#DEBUG=1
+
+# Must be one of "curl" and "ldapsearch".
+# NOTE:
+# - When choosing "curl", make sure "curl --version | grep ldap" outputs
+#   something. Otherwise, curl was compiled without LDAP support.
+# - When choosing "ldapsearch", make sure the ldapwhoami command is
+#   available as well, as that might be needed in some cases.
+#CLIENT="curl"
+
+# Usernames should be validated using a regular expression to be of
+# a known format. Special characters will be escaped anyway, but it is
+# generally not recommended to allow more than necessary.
+# This pattern is set by default. In your config file, you can either
+# overwrite it with a different one or use "unset USERNAME_PATTERN" to
+# disable validation completely.
+USERNAME_PATTERN='^[a-z|A-Z|0-9|_|-|.]+$'
+
+# Adapt to your needs.
+#SERVER="ldap://ldap-server:389"
+# Will try binding as this user.
+# ldap_dn_escape escapes special characters in strings to make them
+# usable within LDAP DN components.
+#USERDN="uid=$(ldap_dn_escape "$username"),ou=people,dc=example,dc=com"
+
+# If you want to take additional checks like requiring group memberships
+# or fetch specific user attributes, you can execute a custom search, which
+# has to return exactly one result in order for authentication to succeed.
+# Uncomment the following lines to enable search query execution.
+#BASEDN="$USERDN"
+#SCOPE="base"
+#FILTER="(&(objectClass=person)(memberOf=cn=some-group,ou=groups,dc=example,dc=com))"
+# Space-separated list of additional LDAP attributes to query.
+# You could process them in your own on_auth_success hook.
+#ATTRS="cn"
+
+# When the timeout (in seconds) is exceeded (e.g. due to slow networking),
+# authentication fails.
+TIMEOUT=3
+
+########## END OF CONFIGURATION ##########
+
+
+########## SCRIPT CODE FOLLOWS, DON'T TOUCH!  ##########
+
 # Log messages to stderr.
 log() {
     echo "$1" >&2
@@ -115,6 +215,15 @@ entries=0
 if [ $result -eq 0 ] && [ ! -z "$BASEDN" ]; then
     entries=$(echo "$output" | grep -cie '^dn\s*:')
     [ "$entries" != "1" ] && result=1
+fi
+
+if [ ! -z "$DEBUG" ]; then
+    cat >&2 <<-EOF
+        Result: $result
+        Number of entries: $entries
+        Client output:
+        $output
+        EOF
 fi
 
 if [ $result -ne 0 ]; then
